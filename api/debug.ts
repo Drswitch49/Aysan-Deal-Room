1dc8980a9aa65ca35c8d927a1a7b8efb319327e2
Kreativesam <realoneade8@gmail.com>
2026-05-28 05:47:13 -0700

    debug: import airtable utils to test for import-time crashes

--- api/debug.ts
+++ api/debug.ts
@@ -1,8 +1,11 @@
+import { TABLES, airtableFetch } from "./_utils/airtable";
+
 export default async function handler(req: any, res: any) {
   const apiKey = process.env.VITE_AIRTABLE_API_KEY || process.env.AIRTABLE_API_KEY;
   const baseId = process.env.VITE_AIRTABLE_BASE_ID || process.env.AIRTABLE_BASE_ID;
   return res.status(200).json({
     nodeVersion: process.version,
+    tablesConstant: TABLES,
     hasApiKey: !!apiKey,
     apiKeyLength: apiKey ? apiKey.length : 0,
     apiKeyPrefix: apiKey ? apiKey.substring(0, 5) : "",
