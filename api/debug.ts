eb891a8352df695560c4fdbab9bfb44591f557a1
Kreativesam <realoneade8@gmail.com>
2026-05-28 07:13:07 -0700

    debug: add pipeline schema lookup

--- api/debug.ts
+++ api/debug.ts
@@ -4,13 +4,11 @@ export default async function handler(req: any, res: any) {
   const apiKey = process.env.VITE_AIRTABLE_API_KEY || process.env.AIRTABLE_API_KEY;
   const baseId = process.env.VITE_AIRTABLE_BASE_ID || process.env.AIRTABLE_BASE_ID;
   
-  let lendersSchema: any = null;
-  let assignmentsSchema: any = null;
+  let pipelineSchema: any = null;
   let metadataError: any = null;
 
   try {
-    lendersSchema = await getTableSchema(TABLES.LENDERS);
-    assignmentsSchema = await getTableSchema(TABLES.ASSIGNMENTS);
+    pipelineSchema = await getTableSchema(TABLES.PIPELINE);
   } catch (err: any) {
     metadataError = {
       message: err.message,
@@ -21,8 +19,7 @@ export default async function handler(req: any, res: any) {
 
   return res.status(200).json({
     nodeVersion: process.version,
-    lendersSchema,
-    assignmentsSchema,
+    pipelineSchema,
     metadataError,
     hasApiKey: !!apiKey,
     apiKeyLength: apiKey ? apiKey.length : 0,
