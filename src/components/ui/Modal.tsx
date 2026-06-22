d19b09ccb3d524e1fa64f805b5910e2cd50fac5d
Drswitch49 <Drswitch49@users.noreply.github.com>
2026-06-22 16:14:01 -0700

    Fix flex-1 scroll sizing and reduce maxHeight

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -60,7 +60,7 @@ export function Modal({
       {/* Modal Panel */}
       <div
         className={`relative z-10 w-full ${maxWidth} flex flex-col rounded-2xl border border-white/[0.1] bg-[#161B22] shadow-2xl animate-scale-in overflow-hidden`}
-        style={{ maxHeight: 'calc(100vh - 2rem)' }}
+        style={{ maxHeight: '90vh' }}
       >
         {/* Header */}
         <div className="flex items-center justify-between p-6 pb-4 border-b border-white/[0.02] shrink-0">
@@ -81,7 +81,7 @@ export function Modal({
         </div>
 
         {/* Body */}
-        <div className="p-6 pt-5 overflow-y-auto custom-scrollbar min-h-0">
+        <div className="p-6 pt-5 overflow-y-auto custom-scrollbar flex-1 min-h-0">
           {children}
         </div>
       </div>
